3c5e62325946953af731d3c2b35d9d98e00d1800
ra2y <tranracheln@gmail.com>
2026-04-25 20:48:40 -0700

    create installer script and internal davinci script

---
+++ BeatFrameScript.py
@@ -0,0 +1,26 @@
+import csv
+
+# get timeline object
+resolve = app.GetResolve()
+pm = resolve.GetProjectManager()
+project = pm.GetCurrentProject()
+timeline = project.GetCurrentTimeline() 
+
+CSV_FILEPATH = "c:\Users\Ray\projects\hackathons\BeatFrame\timestamps.csv"
+with open(CSV_FILEPATH, mode='r', newline='') as file:
+    reader = csv.reader(file)
+    
+    for row in reader:
+        if not row:
+            continue
+        try:
+            timestamp = float(row[0])
+            
+            # convert timestamps to frame by getting the fps of project
+            fps = float(timeline.GetSetting("timelineFrameRate"))
+            frame = round(timestamp * fps)
+
+            timeline.AddMarker(frame, 'Blue', 'Important Beat', 'Detailed Note here', 1)
+        except ValueError:
+            print(f"Skipping invalid value: {row[0]}")
+
---
+++ installer.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import shutil
+import platform
+
+# Directory where this script lives
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Build path to your file
+csv_filepath = os.path.join(script_dir, "timestamps.csv")
+
+script_filepath = "BeatFrameScript.py"
+
+with open(script_filepath, "r") as f:
+    lines = f.readlines()
+
+# change the filepath inside the script to be the user's specific filepath
+# to the csv file
+for i, line in enumerate(lines):
+    if line.strip().startswith("CSV_FILEPATH ="):
+        lines[i] = "CSV_FILEPATH = \"" + csv_filepath + "\"\n"   # new line
+
+with open(script_filepath, "w") as f:
+    f.writelines(lines)
+
+# find davinci resolve filepath of the user
+def get_resolve_scripts_path():
+    home = Path.home()
+    system = platform.system()
+
+    if system == "Darwin":  # macOS
+        return home / "Library/Application Support/Blackmagic Design/DaVinci Resolve/Fusion/Scripts"
+    elif system == "Windows":
+        return home / "AppData/Roaming/Blackmagic Design/DaVinci Resolve/Support/Fusion/Scripts"
+    elif system == "Linux":
+        return home / ".local/share/DaVinciResolve/Fusion/Scripts"
+    else:
+        raise Exception("Unsupported OS")
+
+def install_script(source_file):
+    dest_dir = get_resolve_scripts_path()
+    dest_dir.mkdir(parents=True, exist_ok=True)
+
+    dest_file = dest_dir / Path(source_file).name
+    shutil.copy2(source_file, dest_file)
+
+    print(f"Copied to: {dest_file}")
+
+# install script to davinci resolve of user
+install_script("BeatFrameScript.py")
