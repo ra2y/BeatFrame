e8207a862356fcd3a325861a776948a689ecc691
Cindy Tat <cindyannetat@gmail.com>
2026-04-26 00:27:28 -0700

    updated main.py and added app icon

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from ui import BeatFrameApp
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 import sys
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon("soundicon.png"))
+
 window = BeatFrameApp()
 window.show()
 sys.exit(app.exec())
